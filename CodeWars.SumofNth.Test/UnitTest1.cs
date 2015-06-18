3b466919bc453c0bd3cfe497efe902624e51c04f
eddy556 <eddy556@gmail.com>
2015-06-19 00:12:27 +0100

    Final submission

---
+++ CodeWars.SumofNth.Test/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars.SumofNth.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+ 
+        [TestMethod]
+            public void TestZero_ExpectZero() {
+            Assert.AreEqual("0.00", NthSeries.seriesSum(0));
+        }
+
+        [TestMethod]
+        public void Test1_Expect1()
+        {
+            Assert.AreEqual("1.00", NthSeries.seriesSum(1));
+        }
+
+        [TestMethod]
+        public void Test2_Expect()
+        {
+            Assert.AreEqual("1.25", NthSeries.seriesSum(2));
+        }
+
+        [TestMethod]
+        public void Test3_Expect()
+        {
+            Assert.AreEqual("1.39", NthSeries.seriesSum(3));
+        }
+
+        [TestMethod]
+        public void Test5_Expect()
+        {
+            Assert.AreEqual("1.57", NthSeries.seriesSum(5));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual("1.77", NthSeries.seriesSum(9));
+        }
+  
+
+    }
+}
---
+++ CodeWars.SumofNth/NthSeries.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class NthSeries
+{
+    public static string seriesSum(int n)
+    {
+        // Happy Coding ^_^
+
+        if (n == 0)
+            return "0.00";
+
+        decimal total = 0;
+
+        for(int i=1; i<=n; i++)
+        {
+            decimal division = 1 / FindDivider(i);
+            total = total + division;
+        }
+
+        return total.ToString("F");
+
+    }
+
+    private static decimal FindDivider(int n)
+    {
+        int initialDivder = 4;
+
+        if (n == 1)
+            return 1;
+
+        if (n == 2)
+            return initialDivder;
+
+        return initialDivder + (3 * (n - 2));
+
+    }
+}
